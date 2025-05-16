c1e842dbdf246df47cd003465d33e04ee3341b5f
unknown <aeledeisy5@gmail.com>
2025-05-16 18:42:54 +0300

    update

--- Webb/scripts/products.js
+++ Webb/scripts/products.js
@@ -1,4 +1,5 @@
 const productsGrid = document.getElementById("products-grid");
+const searchInput = document.getElementById("search");
 
 async function fetchingProducts(category) {
     try {
@@ -9,8 +10,34 @@ async function fetchingProducts(category) {
             showToast(products.message || "Failed to load products", "danger");
             return;
         }
+        search(products);
+        renderProducts(products);
 
-        productsGrid.innerHTML = "";
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        showToast("Error fetching products", "danger");
+    }
+}
+function search(products){
+searchInput.addEventListener("input", () => {
+    const keyword = searchInput.value.trim().toLowerCase();
+    const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(keyword)
+        || product.description?.toLowerCase().includes(keyword)
+    );
+    renderProducts(filteredProducts);
+    if (filteredProducts.length) {
+        
+    }
+});
+}
+
+function renderProducts(products) {
+     if (products.length === 0) {
+        productsGrid.innerHTML = "<p class='no-products'>No products found.</p>";
+        return;
+    }
+    productsGrid.innerHTML = "";
 
         products.forEach(product => {
             const productCard = document.createElement("div");
@@ -25,11 +52,7 @@ async function fetchingProducts(category) {
             `;
             productsGrid.appendChild(productCard);
         });
-
-    } catch (error) {
-        console.error("Error fetching products:", error);
-        showToast("Error fetching products", "danger");
-    }
+    
 }
 
 async function addToCart(productId) {
